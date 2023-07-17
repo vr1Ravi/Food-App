db301c4a99117993f2a01353d6e8c24549ecba7f
Ravi <ravikumarjha059@gmail.com>
2023-07-17 12:38:47 +0530

    Modified js and css

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const radomMeal = document.getElementById("randomimg");
-const searchInput = document.getElementById("search-item");
 const searchBtn = document.getElementById("search-reciepe");
+const searchInput = document.getElementById("search-item");
+const searchedMeals = document.querySelector(".searchedMeals");
+const left = document.querySelector(".left");
+let selectedMeal = "";
+
+{
+  /* <ul>
+        
+
+${
+  mealByIdData.strIngredient1 !== "" && (
+    <li>
+      <i class="fa-solid fa-star-half-stroke fa-spin"></i>
+      mealByIdData.strIngredient1
+    </li>
+  )
+}
+
+<li>
+  <i class="fa-solid fa-star-half-stroke fa-spin"></i>Ingr12345
+</li>
+<li>
+  <i class="fa-solid fa-star-half-stroke fa-spin"></i>Ingr12345
+</li>
+</ul> */
+}
+
+// console.log(logo.children[0].id);
 // Getting the random meal from the API
 const getRandomMeal = async () => {
   const res = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`);
   const randomMealData = await res.json();
-
   loadRandomMeal(randomMealData.meals[0]);
 };
 
@@ -48,7 +74,7 @@ async function loadRandomMeal(randomMealData) {
     // getRandomMeal();
   });
 }
-// Calling the getRandomMeal function
+// Calling the getRandomMeal functions
 getRandomMeal();
 
 // Getting the meal by ID from the API
@@ -57,18 +83,82 @@ const getMealById = async (id) => {
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=` + id
   );
   const mealByIdData = await res.json();
-  return mealByIdData.meals[0];
+  console.log(mealByIdData.meals[0]);
+  loadMealById(mealByIdData.meals[0]);
 };
-
+function loadMealById(mealByIdData) {
+  left.innerHTML = `<h1>${mealByIdData.strMeal}</h1>
+    <div class="favMeal">
+      <img
+        class="meal-img"
+        src=${mealByIdData.strMealThumb}
+        alt="meal-img"
+      />
+    </div>
+    <div class="mealIngredieantInfo">
+    
+      <p>
+         ${mealByIdData.strInstructions}
+      </p>
+      <a href=${mealByIdData.strYoutube}>Watch Tutorial<i class="fa-brands fa-youtube"></i></a>
+    </div>`;
+  const mealIngredieantInfo = left.children[2];
+  const ul = document.createElement("ul");
+  ul.innerHTML = `<h2><i class="fa-solid fa-arrow-right fa-fade"></i> Ingredients</h2>`;
+  mealIngredieantInfo.prepend(ul);
+  for (let i = 20; i > 0; i--) {
+    if (mealByIdData[`strIngredient${i}`] !== "") {
+      const li = document.createElement("li");
+      li.innerHTML = `<i class="fa-solid fa-star-half-stroke fa-spin"></i>${
+        mealByIdData[`strIngredient${i}`]
+      } - ${mealByIdData[`strMeasure${i}`]}`;
+      ul.append(li);
+    }
+  }
+}
 // Getting the meal by search from the API
 const getMealsBySearch = async (search) => {
   const res = await fetch(
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
   );
   const mealBySearchData = await res.json();
-  console.log(mealBySearchData);
+  // console.log(mealBySearchData);
   return mealBySearchData.meals;
 };
+searchBtn.addEventListener("click", async () => {
+  searchItemValue = searchInput.value;
+  searchedMeals.innerHTML = "";
+  const meals = await getMealsBySearch(searchItemValue);
+
+  if (meals !== null) {
+    meals.map((meal) => {
+      const div = document.createElement("div");
+      div.classList.add("searchedMeal");
+      div.innerHTML = ` <img
+      class="meal-img"
+      id=${meal.idMeal}
+      src= ${meal.strMealThumb}
+      alt="not found"
+    />
+    <p>${meal.strMeal}</p>`;
+      searchedMeals.append(div);
+    });
+    setTimeout(() => {
+      selectedMeal = Array.from(
+        searchedMeals.querySelectorAll(".searchedMeal")
+      );
+      console.log(selectedMeal);
+      selectedMeal.map((item, idx) => {
+        item.addEventListener("click", () => {
+          // console.log(item.children[0].id, idx);
+          getMealById(item.children[0].id);
+        });
+      });
+    }, 1000);
+  } else {
+    alert("No meal found");
+  }
+});
 
 // Function to remove the meal from the Local Storage
 function removeFromLs(mealId) {
