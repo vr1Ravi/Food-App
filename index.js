391ed16ba118c50be2d4030864fc5ff67485e492
Ravi <ravikumarjha059@gmail.com>
2023-07-29 17:50:26 +0530

    recipe page added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const radomMeal = document.getElementById("randomimg");
 const searchBtn = document.getElementById("search-reciepe");
 const searchInput = document.getElementById("search-item");
 const searchedMeals = document.querySelector(".searchedMeals");
-const left = document.querySelector(".left");
+
 let selectedMeal = "";
 
 // Getting the random meal from the API
@@ -26,52 +26,12 @@ async function loadRandomMeal(randomMealData) {
 // Calling the getRandomMeal functions
 getRandomMeal();
 
-// Getting the meal by ID from the API
-const getMealById = async (id) => {
-  const res = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=` + id
-  );
-  const mealByIdData = await res.json();
-  console.log(mealByIdData.meals[0]);
-  loadMealById(mealByIdData.meals[0]);
-};
-function loadMealById(mealByIdData) {
-  left.innerHTML = `<h1>${mealByIdData.strMeal}</h1>
-    <div class="favMeal">
-      <img
-        class="meal-img"
-        src=${mealByIdData.strMealThumb}
-        alt="meal-img"
-      />
-    </div>
-    <div class="mealIngredieantInfo">
-    
-      <p>
-         ${mealByIdData.strInstructions}
-      </p>
-      <a href=${mealByIdData.strYoutube}>Watch Tutorial<i class="fa-brands fa-youtube"></i></a>
-    </div>`;
-  const mealIngredieantInfo = left.children[2];
-  const ul = document.createElement("ul");
-  ul.innerHTML = `<h2><i class="fa-solid fa-arrow-right fa-fade"></i> Ingredients</h2>`;
-  mealIngredieantInfo.prepend(ul);
-  for (let i = 20; i > 0; i--) {
-    if (mealByIdData[`strIngredient${i}`] !== "") {
-      const li = document.createElement("li");
-      li.innerHTML = `<i class="fa-solid fa-star-half-stroke fa-spin"></i>${
-        mealByIdData[`strIngredient${i}`]
-      } - ${mealByIdData[`strMeasure${i}`]}`;
-      ul.append(li);
-    }
-  }
-}
 // Getting the meal by search from the API
 const getMealsBySearch = async (search) => {
   const res = await fetch(
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
   );
   const mealBySearchData = await res.json();
-  // console.log(mealBySearchData);
   return mealBySearchData.meals;
 };
 // Adding the event listener to the search button
@@ -85,7 +45,7 @@ searchBtn.addEventListener("click", async () => {
       const div = document.createElement("div");
       div.classList.add("searchedMeal");
       div.innerHTML = ` <img
-      class="meal-img"
+      class= "meal-img"
       id=${meal.idMeal}
       src= ${meal.strMealThumb}
       alt="not found"
@@ -99,8 +59,10 @@ searchBtn.addEventListener("click", async () => {
       );
       console.log(selectedMeal);
       selectedMeal.map((item, idx) => {
-        item.addEventListener("click", () => {
-          getMealById(item.children[0].id);
+        item.addEventListener("click", async () => {
+          console.log(item.children[0].id);
+          addMealToLs(item.children[0].id);
+          window.location.href = "/recipe.html";
         });
       });
     }, 1000);
@@ -111,18 +73,16 @@ searchBtn.addEventListener("click", async () => {
 
 // Function to remove the meal from the Local Storage
 function removeFromLs(mealId) {
-  const mealIds = getMealsFromLS();
-  const filteredMealIds = mealIds.filter((id) => id !== mealId);
-  localStorage.setItem("mealIds", JSON.stringify([...filteredMealIds]));
+  localStorage.removeItem("mealId");
 }
 // Function to add the meal from the Local Storage
 function addMealToLs(mealId) {
-  const mealIds = getMealsFromLS();
-  localStorage.setItem("mealIds", JSON.stringify([...mealIds, mealId]));
+  removeFromLs();
+  localStorage.setItem("mealId", JSON.stringify(mealId));
 }
 
 // Function to get the meal from the Local Storage
 function getMealsFromLS() {
-  const mealIds = JSON.parse(localStorage.getItem("mealIds"));
-  return mealIds === null ? [] : mealIds;
+  const mealId = JSON.parse(localStorage.getItem("mealId"));
+  return mealId === null ? "Please SelectMeal" : mealId;
 }
---
+++ recipe.js
@@ -0,0 +1,52 @@
+const left = document.querySelector(".left");
+console.log(left);
+
+// Getting the meal by ID from the API
+const getMealById = async (id) => {
+  const res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=` + id
+  );
+  const mealByIdData = await res.json();
+  console.log(mealByIdData.meals[0]);
+  loadMealById(mealByIdData.meals[0]);
+};
+function loadMealById(mealByIdData) {
+  left.innerHTML = `<h1>${mealByIdData.strMeal}</h1>
+      <div class="favMeal">
+        <img
+          class="meal-img"
+          src=${mealByIdData.strMealThumb}
+          alt="meal-img"
+        />
+        
+      </div>
+      <div class="mealIngredieantInfo">
+        <p>
+           ${mealByIdData.strInstructions}
+        </p>
+        <a href=${mealByIdData.strYoutube}>Watch Tutorial<i class="fa-brands fa-youtube"></i></a>
+      </div>`;
+  const mealIngredieantInfo = left.children[2];
+  const ul = document.createElement("ul");
+  ul.classList.add("ingredieantList");
+  mealIngredieantInfo.prepend(ul);
+  for (let i = 20; i > 0; i--) {
+    if (mealByIdData[`strIngredient${i}`] !== "") {
+      const li = document.createElement("li");
+      li.innerHTML = `<i class="fa-solid fa-star-half-stroke fa-spin"></i>${
+        mealByIdData[`strIngredient${i}`]
+      } - ${mealByIdData[`strMeasure${i}`]}`;
+      ul.append(li);
+    }
+  }
+  const h2 = document.createElement("h2");
+  h2.innerHTML = `<i class="fa-solid fa-arrow-right fa-fade"></i> Ingredients`;
+  mealIngredieantInfo.prepend(h2);
+}
+
+function getMealsFromLS() {
+  const mealId = JSON.parse(localStorage.getItem("mealId"));
+  return mealId === null ? "Please SelectMeal" : mealId;
+}
+const mealId = getMealsFromLS();
+getMealById(mealId);
